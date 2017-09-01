719d08ebf35de05d861116a731964a7f4c1b73b7
Sebastien Ritz <sebastien.ritz@gmail.com>
2017-09-01 11:07:50 +0200

    feat(init): remove lock stuff as it is not the way redis recommends to do so

--- src/module/services/redis-client.service.ts
+++ src/module/services/redis-client.service.ts
@@ -36,44 +36,4 @@ export class RedisClientService {
     public get connection(): HapinessRedisClient {
         return this._client;
     }
-
-    /**
-     *
-     * @param key The key to lock
-     * @param ttl The time before redis delete the key (default value 1000)
-     * @param unit PX means the ttl was given in Seconds and MX means it was given in Milliseconds (default value is PX)
-     *
-     * Will set the key with the ttl only if it does not exists yet
-     */
-    public lock(key: string, ttl: number = 1000, unit: string = 'PX'): Observable<boolean> {
-        const _unit = ['PX', 'MX'].indexOf(unit) > -1 ? unit : 'PX';
-        return this._client.set(key, ttl.toString(), _unit, ttl, 'NX').map(_ => _ === 'OK');
-    }
-
-    /**
-     *
-     * @param key The key to delete in Redis
-     *
-     * Redis will give the number of deleted element and we'll return a boolean saying
-     * TRUE if something has been deleted
-     * FALSE if nothing has been deleted
-     *
-     */
-    public unlock(key: string): Observable<boolean> {
-        return this._client.del(key).map(_ => _ > 0);
-    }
-
-    /**
-     *
-     * @param key The key to check the lock
-     *
-     * Return true if there is a lock on the key else it will return false
-     */
-    public isLocked(key: string): Observable<boolean> {
-        const ttl$ = this._client.ttl(key).map(ttl => Number(ttl));
-        return Observable.merge(
-                ttl$.filter(ttl => ttl === -1).switchMap(_ => this.unlock(key)).map(_ => false),
-                ttl$.filter(ttl => ttl !== -1).map(ttl => ttl >= 0)
-            );
-    }
 }
